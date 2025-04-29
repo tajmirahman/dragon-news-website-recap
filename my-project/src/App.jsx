07b73335c5f624b18c5ec71c85cd4e777e718521
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-04-29 20:38:43 +0600

    dragon new project start here

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -16,8 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className='text-7xl text-red-200'>Vite + React</h1>
-      <button className='btn btn-primary'>hi</button>
+      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ my-project/src/components/MainLayOut.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLayOut = () => {
+    return (
+        <div>
+            <h2>Main layOut</h2>
+        </div>
+    );
+};
+
+export default MainLayOut;
