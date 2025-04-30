d0c8b48ed501ab001ba795fdf54b5549af715f34
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-04-30 21:12:26 +0600

    google font add successfully

--- my-project/src/components/Header/Header.jsx
+++ my-project/src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
             <div className='w-[300px]'>
                 <img src={logo} alt="" />
             </div>
-            <h1 className='text-gray-500'>Journalism Without Fear or Favour</h1>
+            <h1 className='text-gray-500 font-poppins'>Journalism Without Fear or Favour</h1>
             <p>{moment().format('MMMM Do YYYY, h:mm:ss a')}</p>
         </div>
     );
