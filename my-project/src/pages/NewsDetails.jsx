04d13fab6a15b23b15e1a262d7eed3e1c8b0bf6d
Tajmilur Rahman <tajmilurrahman91@gmail.com>
2025-05-03 14:21:38 +0600

    details page done

--- my-project/src/pages/NewsDetails.jsx
+++ my-project/src/pages/NewsDetails.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import Header from '../components/Header/Header';
 import { Link, useLoaderData } from 'react-router-dom';
+import RightSide from '../components/component-layout/RightSide';
 
 
 const NewsDetails = () => {
 
-    const data=useLoaderData();
+    const data = useLoaderData();
 
-    const news=data.data[0];
-
-
+    const news = data.data[0];
 
 
-  
+
+
+
 
     return (
         <div>
@@ -20,11 +21,18 @@ const NewsDetails = () => {
                 <Header></Header>
             </header>
 
-            <div>
-                <img src={news.image_url} alt="" />
-                <h1>{news.title}</h1>
-                <p>{news.details}</p>
-                <Link to={'/'}><button className='btn'>Back To Category</button></Link>
+            <div className=' w-11/12 mx-auto grid md:grid-cols-12 gap-3'>
+                <section className='col-span-9'>
+                    <div className='text-center space-y-2'>
+                        <img className='w-[1000px] h-full' src={news?.image_url} alt="" />
+                        <h1 className='text-xl font-bold'>{news?.title}</h1>
+                        <p>{news?.details}</p>
+                        <Link to={'/'}><button className='btn btn-primary mt-2'>Back To Category</button></Link>
+                    </div>
+                </section>
+                <aside className='col-span-3'>
+                    <RightSide></RightSide>
+                </aside>
             </div>
 
         </div>
